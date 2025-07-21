db661de6df8c06c0a827b9752817b1aaa8f09354
권영재 <dwdjjj@naver.com>
2025-07-21 20:40:11 +0900

    Delete : "custom shpae 적용 안되서 제거"

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,23 +6,17 @@ import Guestbook from "@/components/Guestbook";
 import ShareButtons from "@/components/ShareButtons";
 import Header from "@/components/Header";
 import MapClient from "@/components/MapClient";
-import SectionEffect from "@/effects/SectionEffect";
-import { playWeddingHearts, playFlowerRain } from "@/effects/effects";
 
 export default function Home() {
   return (
     <div className="main-container flexcenter flex-col text-center">
-      <SectionEffect onEnter={() => playFlowerRain(50)} threshold={0.5}>
-        <Header />
-      </SectionEffect>
+      <Header />
       <Contact />
 
       <Gallery />
 
       <Calendar />
-      <SectionEffect onEnter={() => playWeddingHearts(60)} threshold={1}>
-        <Guestbook />
-      </SectionEffect>
+      <Guestbook />
       <MapClient />
       <ShareButtons />
     </div>
